8f1cf49216144158819a80b4cec8df53459d7959
Ilia Filimonov <9991571155i@gmail.com>
2023-01-07 21:41:07 +0700

    added calculator project

---
+++ lessons/lesson01/calculator/src/js/calculator.js
@@ -0,0 +1,18 @@
+export function sum(a, b) {
+	// your code
+}
+
+
+export function subtract(a, b) {
+	// your code
+}
+
+
+export function multiply(a, b) {
+	// your code
+}
+
+
+export function divide(a, b) {
+	// your code
+}
