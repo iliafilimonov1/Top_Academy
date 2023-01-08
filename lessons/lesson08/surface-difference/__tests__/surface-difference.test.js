44a600c34519c7658cbff8fb84ffaf92ba2031e5
Ilia Filimonov <9991571155i@gmail.com>
2023-01-08 23:23:39 +0700

    fixed tests

--- lessons/lesson08/surface-difference/__tests__/surface-difference.test.js
+++ lessons/lesson08/surface-difference/__tests__/surface-difference.test.js
@@ -2,7 +2,7 @@ import { calculateDifference } from "../src/js/surface-difference";
 
 describe('calculateDifference', () => {
 	it('should return the correct difference between the areas of the two rectangles', () => {
-		expect(calculateDifference([2, 3], [3, 4])).toBe(-5)
-		expect(calculateDifference([5, 10], [3, 4])).toBe(42)
+		expect(calculateDifference([2, 3], [3, 4])).toBe(-6);
+		expect(calculateDifference([5, 10], [3, 4])).toBe(38);
 	})
 })
