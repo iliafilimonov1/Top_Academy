0b2bfbdfa2a8f581627e9223cea6b534fea6093e
Ilia Filimonov <9991571155i@gmail.com>
2023-01-08 23:33:50 +0700

    added stats project with tests

---
+++ lessons/lesson09/stats/src/js/stats.js
@@ -0,0 +1,22 @@
+/**
+ * @param {array} grades
+ */
+export function getNumberOfGrades(grades) {
+	// your code
+}
+
+
+/**
+ * @param {array} grades
+ */
+export function getFirstGrade(grades) {
+	// your code
+}
+
+
+/**
+ * @param {array} grades
+ */
+export function getLastGrade(grades) {
+	// your code
+}
