9aa5485f3aeeeae607e07b4cf2e5c6cc0dc87f25
Ilia Filimonov <9991571155i@gmail.com>
2023-01-08 13:56:23 +0700

    added name variations project with tests

---
+++ lessons/lesson02/name-variations/src/js/helpers.test.js
@@ -0,0 +1,25 @@
+import { getNumberOfChars, getLower, getUpper } from "./helpers.js";
+
+describe('getNumberOfChars function', () => {
+	test('should return the number of characters in a string', () => {
+		expect(getNumberOfChars('hello')).toBe(5);
+		expect(getNumberOfChars('12345')).toBe(5);
+		expect(getNumberOfChars('')).toBe(0);
+	});
+});
+
+describe('getLower function', () => {
+	test('should return the lowercase version of a string', () => {
+		expect(getLower('HELLO')).toBe('hello');
+		expect(getLower('12345')).toBe('12345');
+		expect(getLower('')).toBe('');
+	});
+});
+
+describe('getUpper function', () => {
+	test('should return the uppercase version of a string', () => {
+		expect(getUpper('hello')).toBe('HELLO');
+		expect(getUpper('12345')).toBe('12345');
+		expect(getUpper('')).toBe('');
+	});
+});
