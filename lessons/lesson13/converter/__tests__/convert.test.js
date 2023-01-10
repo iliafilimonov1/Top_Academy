b23fbfb0758b03ca072378d34343ab333fa0e214
Ilia Filimonov <9991571155i@gmail.com>
2023-01-10 16:40:43 +0700

    delete unusable tests

--- lessons/lesson13/converter/__tests__/convert.test.js
+++ lessons/lesson13/converter/__tests__/convert.test.js
@@ -6,28 +6,6 @@ describe('convertKMtoMiles', () => {
 	});
 });
 
-describe('kmInput event listener', () => {
-	let kmInput;
-	let milesOutput;
-
-	beforeEach(() => {
-		// Set up the DOM
-		document.body.innerHTML = `
-		<input id="km-input" value="1">
-		<input id="miles-output">
-	  `;
-		kmInput = document.querySelector('#km-input');
-		milesOutput = document.querySelector('#miles-output');
-	});
-
-	it('updates the milesOutput element with the converted value', () => {
-		// Trigger the keyup event on the kmInput element
-		kmInput.dispatchEvent(new Event('keyup'));
-
-		expect(milesOutput.value).toBeCloseTo(0.62137);
-	});
-});
-
 
 
 
