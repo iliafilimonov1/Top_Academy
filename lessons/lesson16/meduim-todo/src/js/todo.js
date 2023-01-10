3ab5649f07a792153fc713ba813d2e5086eee270
Ilia Filimonov <9991571155i@gmail.com>
2023-01-10 17:23:28 +0700

    added medium-todo with tests

---
+++ lessons/lesson16/meduim-todo/src/js/todo.js
@@ -0,0 +1,44 @@
+/**
+ * @typedef {{title: String, isCompleted: Boolean, icon: String, category: String, author: Object}} Todo
+ *
+ * @param {object} data
+ */
+export function createTodo(data) {
+	// your code
+}
+
+/**
+ * @param {Todo[]} todos
+ * @param {Todo} todo
+ */
+export function addTodo(todos, todo) {
+	// your code
+}
+
+/**
+ * @param {Todo[]} todos
+ */
+export function countTodos(todos) {
+	// your code
+}
+
+/**
+ * @param {Todo[]} todos
+ */
+export function firstTodoTitle(todos) {
+	// your code
+}
+
+/**
+ * @param {Todo[]} todos
+ */
+export function lastTodoTitle(todos) {
+	// your code
+}
+
+/**
+ * @param {Todo[]} todos
+ */
+export function exportTitles(todos) {
+	// your code
+}
