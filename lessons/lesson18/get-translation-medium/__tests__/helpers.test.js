5136cf2c594760ac4be3e0840aafe6999484ad46
Ilia Filimonov <9991571155i@gmail.com>
2023-01-11 14:28:29 +0700

    fixed tests

--- lessons/lesson18/get-translation-medium/__tests__/helpers.test.js
+++ lessons/lesson18/get-translation-medium/__tests__/helpers.test.js
@@ -1,41 +1,41 @@
 import { getTranslation } from "../src/js/helpers.js";
 
-describe('getTranslation', () => {
-	test('returns the translation for Dutch', () => {
-		expect(getTranslation('dutch')).toBe('Welkom');
+describe("getTranslation", () => {
+	test("returns the translation for Dutch", () => {
+		expect(getTranslation("dutch")).toBe("Welkom");
 	});
 
-	test('returns the translation for French', () => {
-		expect(getTranslation('french')).toBe('Bienvenue');
+	test("returns the translation for French", () => {
+		expect(getTranslation("french")).toBe("Bienvenue");
 	});
 
-	test('returns the translation for English', () => {
-		expect(getTranslation('english')).toBe('Welcome');
+	test("returns the translation for English", () => {
+		expect(getTranslation("english")).toBe("Welcome");
 	});
 
-	test('returns the translation for Spanish', () => {
-		expect(getTranslation('spanish')).toBe('Bienvenidos');
+	test("returns the translation for Spanish", () => {
+		expect(getTranslation("spanish")).toBe("Bienvenidos");
 	});
 
-	test('returns the translation for Italian', () => {
-		expect(getTranslation('italian')).toBe('Ben arrivato');
+	test("returns the translation for Italian", () => {
+		expect(getTranslation("italian")).toBe("Ben arrivato");
 	});
 
-	test('returns the translation for Korean', () => {
-		expect(getTranslation('korean')).toBe('어서 오십시오');
+	test("returns the translation for Korean", () => {
+		expect(getTranslation("korean")).toBe("어서 오십시오");
 	});
 
 	test('returns "welcome" when language is not present', () => {
-		expect(getTranslation('portuguese')).toBe('welcome');
+		expect(getTranslation("portuguese")).toBe("Welcome");
 	});
 
 	test('returns "welcome" when language is null or undefined', () => {
-		expect(getTranslation(null)).toBe('welcome');
-		expect(getTranslation(undefined)).toBe('welcome');
+		expect(getTranslation(null)).toBe("Welcome");
+		expect(getTranslation(undefined)).toBe("Welcome");
 	});
 
-	test('returns expected value is of type string', () => {
-		expect(typeof (getTranslation('italian'))).toBe('string');
+	test("returns expected value is of type string", () => {
+		expect(typeof getTranslation("italian")).toBe("string");
 	});
 });
 
