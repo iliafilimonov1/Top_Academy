155647f9a96ca9441fd3506f33305d38d984004f
Ilia Filimonov <9991571155i@gmail.com>
2023-01-31 16:49:51 +0700

    added todos project with tests

---
+++ lessons/lesson31/todo-hard/__tests__/todos.test.js
@@ -0,0 +1,41 @@
+import { Todos } from "../src/js/todos.js";
+
+describe("Todos", () => {
+	let todos;
+
+	beforeEach(() => {
+		todos = new Todos();
+	});
+
+	test("getAll returns all todos", () => {
+		expect(todos.getAll().length).toBe(2);
+	});
+
+	test("getCount returns the correct number of todos", () => {
+		expect(todos.getCount()).toBe(2);
+	});
+
+	test("add adds a new todo", () => {
+		todos.add("Buy groceries", "personal");
+		expect(todos.getCount()).toBe(3);
+	});
+
+	test("getWork returns only work todos", () => {
+		expect(todos.getWork().length).toBe(1);
+		expect(todos.getWork()[0].category).toBe("work");
+	});
+
+	test("getWorkCount returns the correct number of work todos", () => {
+		expect(todos.getWorkCount()).toBe(1);
+	});
+
+	test("getPersonal returns only personal todos", () => {
+		expect(todos.getPersonal().length).toBe(1);
+		expect(todos.getPersonal()[0].category).toBe("personal");
+	});
+
+	test("getPersonalCount returns the correct number of personal todos", () => {
+		expect(todos.getPersonalCount()).toBe(1);
+	});
+});
+
---
+++ lessons/lesson31/todo-hard/src/js/todos.js
@@ -0,0 +1,41 @@
+export default class Todos {
+  constructor() {
+    // we don't capture any parameters here
+    // we're defining an array of todos with two example todos
+    this.todos = [{
+      title: "Learn JavaScript",
+      category: "work"
+    }, {
+      title: "Meditate",
+      category: "personal"
+    }];
+  }
+
+  getAll() {
+    // your code
+  }
+
+  getCount() {
+    // your code
+  }
+
+  add(title, category) {
+    // your code
+  }
+
+  getWork() {
+    // your code
+  }
+
+  getWorkCount() {
+    // your code
+  }
+
+  getPersonal() {
+    // your code
+  }
+
+  getPersonalCount() {
+    // your code
+  }
+}
