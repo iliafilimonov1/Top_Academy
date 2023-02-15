02875ed923ecc12207ad65f6efb964c36a17e48f
Ilia Filimonov <9991571155i@gmail.com>
2023-02-15 12:32:25 +0700

    added examples for work with promises

---
+++ lessons/lesson39/promises-three/src/js/script.js
@@ -0,0 +1,13 @@
+import { wait } from "./helpers.js";
+
+const waitPromise = wait(3000);
+console.log(waitPromise); // pending 
+
+waitPromise.then(() => {
+  console.log("waited 3 second");
+  console.log(waitPromise); // fulfilled 
+});
+
+console.log(waitPromise); // pending 
+
+
