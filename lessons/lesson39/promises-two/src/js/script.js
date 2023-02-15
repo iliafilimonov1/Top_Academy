1abc219211e2a412b6ebf7b3f37695d32a4a23bd
Ilia Filimonov <9991571155i@gmail.com>
2023-02-15 12:25:34 +0700

    added examples for work with promises

---
+++ lessons/lesson39/promises-two/src/js/script.js
@@ -0,0 +1,13 @@
+import { wait } from "./helpers.js";
+
+const init = () => {
+  console.log("Program started");
+  wait(3000);
+  console.log("Waited 3 second");
+}
+
+
+// Sample usage (do not modify)
+init();
+
+
