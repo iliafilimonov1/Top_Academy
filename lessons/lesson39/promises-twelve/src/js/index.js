6aaebd695da9ba5b1915c5fc2e0419048b95929c
Ilia Filimonov <9991571155i@gmail.com>
2023-02-15 17:09:58 +0700

    added examples for work with promises

---
+++ lessons/lesson39/promises-twelve/src/js/index.js
@@ -0,0 +1,13 @@
+const checkForNewNotifications = () => {
+  // your code
+}
+
+
+// do NOT modify this function
+function showNewNotifications(count) {
+  console.log(`You have ${count} new notifications. Would you like to read them?`);
+}
+
+
+// Sample usage - do not modify
+checkForNewNotifications(); // You have 3 new notifications. Would you like to read them?
