821c25860486717e343c70cd6403b268c28c5558
Ilia Filimonov <9991571155i@gmail.com>
2023-02-24 15:35:50 +0800

    add get elements project

---
+++ lessons/lesson41/get-elements/src/js/index.js
@@ -0,0 +1,22 @@
+const getNavbarElement = () => {
+  // TODO: get the element with id navbar
+}
+
+const getMainElement = () => {
+  // TODO: get the element with id main
+}
+
+const getAboutFromFooter = () => {
+  // TODO: get the the about link that's in the footer
+}
+
+const getTheParagraphElement = () => {
+  // TODO: get the paragraph in #main
+}
+
+
+// Sample usage - do not modify
+getNavbarElement();
+getMainElement();
+getAboutFromFooter();
+getTheParagraphElement();
