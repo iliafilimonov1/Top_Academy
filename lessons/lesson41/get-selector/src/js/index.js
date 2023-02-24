a2b0a51327c6308e5178f760b18b0f2ac14e8d78
Ilia Filimonov <9991571155i@gmail.com>
2023-02-24 15:54:00 +0800

    add get selector project

---
+++ lessons/lesson41/get-selector/src/js/index.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} cssSelector
+ */
+const getTextFromSelector = cssSelector => {
+  // your code
+}
+
+
+// Sample usage - do not modify
+getTextFromSelector("h1"); // "First human lands on Mars!"
+getTextFromSelector("h2"); // "element not found"
