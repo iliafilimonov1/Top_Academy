8e96080ed22edbae861f9f4719446a205655719e
Ilia Filimonov <9991571155i@gmail.com>
2023-02-24 16:11:56 +0800

    add get selelctors project

---
+++ lessons/lesson41/get-selectors/src/js/index.js
@@ -0,0 +1,17 @@
+const getDivElements = () => {
+  // TODO: return all the div elements
+};
+
+const getFooterLinks = () => {
+  // TODO: return all the links that are in the footer-wrapper
+};
+
+const getAboutLinks = () => {
+  // TODO: return all the links that point to the About Us page
+};
+
+
+// Sample usage - do not modify
+getDivElements();
+getFooterLinks();
+getAboutLinks();
